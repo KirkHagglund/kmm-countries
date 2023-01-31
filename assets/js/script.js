ce23cfb4897b645dd1e754471e6eb5074bd007c4
mojsun <m.khorashahi7@gmail.com>
2023-01-31 13:38:43 -0500

    java script file added for API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-function test();
+async function searchWikipedia(searchQuery) {
+  const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
+  const response = await fetch(endpoint);
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  const json = await response.json();
+  return json;
+}
