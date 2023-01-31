05820db69c1e46b763d54cc3280a86b01ac18efb
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-01-31 11:20:58 -0800

    changed size of globe div

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,33 @@ const map = new mapboxgl.Map({
     container: 'map', // Container ID
     style: 'mapbox://styles/mapbox/dark-v10', // Map style to use
     projection: 'globe',
-    zoom: 2, // Starting zoom level
+    zoom: 1, // Starting zoom level
     center: [-90, 40]
 });
 
 map.on('style.load', () => {
     map.setFog({}); // Set the default atmosphere style
 });
+
+//---New marker on search input - geocoder ---//
+
+const geocoder = new MapboxGeocoder({
+    // Initialize the geocoder
+    accessToken: mapboxgl.accessToken, // Set the access token
+    mapboxgl: mapboxgl, // Set the mapbox-gl instance
+    //can I get a html element to input
+    marker: false, // Do not use the default marker style
+    placeholder: 'Search', // Placeholder text for the search bar
+});
+
+// Add the geocoder to the map
+map.addControl(geocoder);
+
+// Listen for the `result` event from the Geocoder // `result` event is triggered when a user makes a selection
+//Add a marker at the result's coordinates
+geocoder.on('result', (event) => {
+    map.getSource('single-point').setData(event.result.geometry);
+});
+
 
 //--- END of GLOBE JAVASCRIPT ---//
