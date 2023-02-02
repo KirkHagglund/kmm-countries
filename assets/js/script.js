8a0929ce2ab5f57110e5e05ee4823c1f70429447
elkirko <kirkhagglund@gmail.com>
2023-02-02 13:46:53 -0600

    Fixed 5 entry save limit

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,17 @@ let resultCard = document.getElementById('result-card');
 let wikiDiv = document.getElementById('wiki-link');
 let homeBtn = document.getElementById('refresh');
 resultCard.hidden = true
-let localStorageArray = JSON.parse(localStorage.getItem('cities')) || [];
+let localStorageArray = JSON.parse(localStorage.getItem('countries')) || [];
 let pastCard = document.getElementById('past-aside');
 let countryArray = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anitgua and Barbuda', 'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cote dIvoire', 'Cabo Verde', 'Cambodia', 'Cameroon', 'Canada', 'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros', 'Congo', 'Costa Rica', 'Croatia', 'Cuba', 'Cyprus', 'Czechia', 'Democratic Republic of the Congo', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Eswatini', 'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Greece', 'Grenada', 'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Holy See', 'Honduras', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Mauritania', 'Mauritius', 'Mexico', 'Micronesia', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'North Korea', 'North Macedonia', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Palestine State', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Qatar', 'Romania', 'Russia', 'Rwanda', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent and the Grenadines', 'Samoa', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Korea', 'South Sudan', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Sweden', 'Switzerland', 'Syria', 'Tajikistan', 'Tanzania', 'Thaliand', 'Timor-Leste', 'Togo', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'TUrkey', 'Turkmenistan', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States of America', 'Uruguay', 'Uzbekistan', 'Vanuata', 'Venezuela', 'Vietnam', 'Yemen', 'Zambia', 'Zimbabwe'];
+let alertModal = document.getElementById('modal-one');
+alertModal.hidden = true;
 
 //Overall function calling all APIs
 
 var submitRequest = function (event) {
     event.preventDefault();
     var countryInput = countryInp.value;
-
      if (countryArray.includes(countryInput)) {
         getWikiLink(countryInput);
         restUrl(countryInput);
@@ -26,7 +27,7 @@ var submitRequest = function (event) {
         resultCard.hidden = false;
     }
     else {
-        alert('please enter a country name'); //Need to replace alert with a modal window
+      
     };
     countryInp.value = "";
 };
@@ -137,12 +138,15 @@ const getWikiLink = (countryName) => {
 
                         // Local Storage Section
                         let pastCity = data[0];
+                        if (localStorageArray.length == 5) {
+                            localStorageArray.shift(0);
+                        };
                         localStorageArray.push(pastCity);
-                        localStorage.setItem('cities', JSON.stringify(localStorageArray));
+                        localStorage.setItem('countries', JSON.stringify(localStorageArray));
                         const button = document.createElement("button");
                         button.textContent = data[0];
                         button.setAttribute('class', 'search-card');
-                        pastCard.appendChild(button);
+                        getLocalStorage()
                     });
             };
         });
@@ -207,6 +211,7 @@ const recallRestUrl = function (country) {
 // --- Local Storage Section --- //
 
 const getLocalStorage = function() {
+    pastCard.innerHTML = "";
     for (i = 0; i < localStorageArray.length; i++) {
         const button = document.createElement("button");
                 button.textContent = localStorageArray[i];
